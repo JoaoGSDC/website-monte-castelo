d08a23c50f070af5b17b365376870279c17bcc64
JoaoGSDC <joaogsdc@gmail.com>
2025-06-25 23:45:06 -0300

    fix: change header icon to whatsapp contact

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,15 +5,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import {
-  FaFacebook,
-  FaInstagram,
-  FaMagnifyingGlass,
-  FaMapLocationDot,
-  FaRegMessage,
-  FaBars,
-  FaWhatsapp,
-} from 'react-icons/fa6';
+import { FaFacebook, FaInstagram, FaMapLocationDot, FaRegMessage, FaBars, FaWhatsapp } from 'react-icons/fa6';
 import { scrollToSection } from '@/utils/scrollToSection';
 import { IoClose } from 'react-icons/io5';
 
@@ -79,7 +71,12 @@ const Navbar = () => {
           </div>
 
           <button>
-            <FaMagnifyingGlass />
+            <a
+              href="https://wa.me/5519974102924?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20os%20cursos%20fornecidos%20pela%20Academia%20Monte%20Castelo!"
+              target="_blank"
+            >
+              <FaWhatsapp />
+            </a>
           </button>
         </div>
       </div>

a81a979f3b3ee392948303088328468f456725f4
JoaoGSDC <joaogsdc@gmail.com>
2025-07-02 12:22:31 -0300

    fix: blog search function

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -24,7 +24,7 @@ const fetchPosts = async (search?: string) => {
 };
 
 const Blog = async ({ searchParams }: any) => {
-  const search = searchParams.search || '';
+  const search = searchParams?.search ?? '';
   const content = await fetchPosts(search);
 
   return (
